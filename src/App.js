d42e5197607e97a33a49ce91c11755205f9d24b1
Splashganger <splashgang@gmx.de>
2022-03-03 13:21:45 +0100

    erster build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
-function App() {
+export default function NasaPhoto() {
+  const [nData, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+
+    async function fetchData() {
+      const res = await fetch(
+                // we'll update the KEYHERE soon!
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
+      );
+      const data = await res.json();
+      setData(data);
+    }
+  }, []);
+
+  if (!nData) return <div />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nasa-photo">
+        <img
+          src={nData.url}
+          alt={nData.title}
+          className="photo"
+        />
+      <div>
+        <h1>{nData.title}</h1>
+        <p className="date">{nData.date}</p>
+        <p className="explanation">{nData.explanation}</p>
+      </div>
     </div>
   );
 }
-
-export default App;
